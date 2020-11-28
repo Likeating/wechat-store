6b63a51af28b5afa5d8e3b579fbe33d6eda4b7ec
TigerMan0106 <1390660786@qq.com>
2020-11-28 23:21:00 +0800

    完成了OrderDetail、OrderInfo、PropertyKey、PropertyValue、Sku五个的Mapper

---
+++ src/main/java/com/fortwelve/wechatstore/mapper/PropertyKeyMapper.java
@@ -0,0 +1,25 @@
+package com.fortwelve.wechatstore.mapper;
+
+import com.fortwelve.wechatstore.pojo.PropertyKey;
+import org.apache.ibatis.annotations.*;
+import java.util.List;
+
+@Mapper
+public interface PropertyKeyMapper {
+
+    @Insert("insert into property_key (key_name,isPicture) values (#{key_name},#{isPicture}})")
+    @Options(useGeneratedKeys = true,keyProperty = "key_id",keyColumn = "key_id")
+    public int addPropertyKey(PropertyKey propertyKey);
+
+    @Update("update property_key set key_name=#{key_name},isPicture=#{isPicture} where key_id=#{key_id}")
+    public int updatePropertyKey(PropertyKey propertyKey);
+
+    @Delete("delete from property_key where key_id=#{id}")
+    public int deletePropertyKeyById(java.math.BigInteger id);
+
+    @Select("select * from property_key where key_id = #{id}")
+    public PropertyKey getPropertyKeyById(java.math.BigInteger id);
+
+    @Select("select * from property_key")
+    public List<PropertyKey> getAllPropertyKey();
+}
---
+++ src/main/java/com/fortwelve/wechatstore/mapper/PropertyValueMapper.java
@@ -0,0 +1,26 @@
+package com.fortwelve.wechatstore.mapper;
+
+import com.fortwelve.wechatstore.pojo.PropertyValue;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface PropertyValueMapper {
+
+    @Insert("insert into category (value_name,picture_id) values (#{value_name},#{picture_id})")
+    @Options(useGeneratedKeys = true,keyProperty = "value_id",keyColumn = "value_id")
+    public int addPropertyValue(PropertyValue propertyValue);
+
+    @Update("update property_value set value_name=#{value_name},picture_id=#{picture_id} where value_id=#{value_id}")
+    public int updatePropertyValue(PropertyValue propertyValue);
+
+    @Delete("delete from property_value where value_id=#{id}")
+    public int deletePropertyValueById(java.math.BigInteger id);
+
+    @Select("select * from property_value where value_id = #{id}")
+    public PropertyValue getPropertyValueById(java.math.BigInteger id);
+
+    @Select("select * from property_value")
+    public List<PropertyValue> getAllPropertyValue();
+}
