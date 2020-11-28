3a3d8205a59a0bd44bc3c6535aeba1a182e41d81
836321778@qq.com <836321778@qq.com>
2020-11-28 18:47:31 +0800

    写完了实体类、CategoryMapper、CustomerMapper、PictureMapper

---
+++ src/main/java/com/fortwelve/wechatstore/mapper/PictureMapper.java
@@ -0,0 +1,27 @@
+package com.fortwelve.wechatstore.mapper;
+
+import com.fortwelve.wechatstore.pojo.Picture;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface PictureMapper {
+
+    @Insert("insert into picture (url) values(#{url})")
+    @Options(useGeneratedKeys = true,keyProperty = "picture_id",keyColumn = "picture_id")
+    public int addPicture(Picture picture);
+
+    @Delete("delete from picture where picture_id=#{picture_id}")
+    public int deletePictureById(java.math.BigInteger id);
+
+    @Select("select * from picture where picture_id = #{id}")
+    public Picture getPictureById(java.math.BigInteger id);
+
+    @Select("select * from picture")
+    public List<Picture> getAllPicture();
+
+    @Update("update picture set url=#{url} where picture_id=#{picture_id}")
+    public int updatePicture(Picture picture);
+
+}
