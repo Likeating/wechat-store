8f2b3dc5f235501073dd71606a623b53d3a56d36
TigerMan0106 <1390660786@qq.com>
2020-12-05 14:20:12 +0800

    完成CategoryController

---
+++ src/main/java/com/fortwelve/wechatstore/controller/CategoryController.java
@@ -0,0 +1,48 @@
+package com.fortwelve.wechatstore.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CategoryController {
+
+    @RequestMapping("/categories")
+    public String categories(){
+        return "{\n" +
+                "  \"message\": [\n" +
+                "    {\n" +
+                "      \"cat_id\": 1,\n" +
+                "      \"cat_name\": \"大家电\",\n" +
+                "      \"cat_pid\": 0,\n" +
+                "      \"cat_level\": 0,\n" +
+                "      \"cat_deleted\": false,\n" +
+                "      \"cat_icon\": \"\",\n" +
+                "      \"children\": [\n" +
+                "        {\n" +
+                "          \"cat_id\": 3,\n" +
+                "          \"cat_name\": \"电视\",\n" +
+                "          \"cat_pid\": 1,\n" +
+                "          \"cat_level\": 1,\n" +
+                "          \"cat_deleted\": false,\n" +
+                "          \"cat_icon\": \"\",\n" +
+                "          \"children\": [\n" +
+                "            {\n" +
+                "              \"cat_id\": 5,\n" +
+                "              \"cat_name\": \"曲面电视\",\n" +
+                "              \"cat_pid\": 3,\n" +
+                "              \"cat_level\": 2,\n" +
+                "              \"cat_deleted\": false,\n" +
+                "              \"cat_icon\": \"https://api-hmugo-web.itheima.net/full/2fb113b32f7a2b161f5ee4096c319afedc3fd5a1.jpg\"\n" +
+                "            }\n" +
+                "          ]\n" +
+                "        }\n" +
+                "      ]\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"meta\": {\n" +
+                "    \"msg\": \"获取成功\",\n" +
+                "    \"status\": 200\n" +
+                "  }\n" +
+                "}";
+    }
+}
