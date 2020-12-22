c75c867af3b9373c95dbe5062b5878ceaf3a30a7
TigerMan0106 <1390660786@qq.com>
2020-12-22 13:33:48 +0800

    完成ManagerController

--- src/main/java/com/fortwelve/wechatstore/controller/CategoryControllers.java
+++ src/main/java/com/fortwelve/wechatstore/controller/CategoryControllers.java
@@ -1,12 +1,26 @@
 package com.fortwelve.wechatstore.controller;
 
+import com.fortwelve.wechatstore.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/category")
 public class CategoryControllers {
+    @Autowired
+    CategoryService categoryService;
+    @PostMapping("/addCategory")
+    public Object addCategory(){
+        Map<String,Object> map = new HashMap<>();
+        Map<String,Object> meta = new HashMap<>();
+        Map<String,Object> msg = new HashMap<>();
+        return map;
+    }
 }
